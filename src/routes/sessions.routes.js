fecd6c9919aa82336c88a194d96377e1ec223862
tiago <tiagoamrein@gmail.com>
2023-03-01 00:03:01 -0300

    deploy

---
+++ src/routes/sessions.routes.js
@@ -0,0 +1,9 @@
+ const { Rotuer, Router } = require("express");
+
+ const SessionsController = require("../controllers/SessionsController");
+ const sessionsController = new SessionsController();
+
+ const sessionsRoutes = Router();
+ sessionsRoutes.post("/", sessionsController.create);
+
+ module.exports = sessionsRoutes
